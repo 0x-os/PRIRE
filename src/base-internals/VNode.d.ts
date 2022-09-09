f8e14e78a1ee5a21b7290f92f5d5358b0cd24ec7
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-09 18:34:03 +0800

    Added base-internal: vnode

--- src/base-internals/VNode.d.ts
+++ src/base-internals/VNode.d.ts
@@ -1,2 +1,13 @@
+// Virtual DOM
+
 export interface VNode<P = {}> {
+    type: ComponentType<P> string;
+    props: P & { children, ComponentChildren };
+    key: Key;
+    ref?: Ref<any> | null;
+    // Start time for the rendering of this vnode
+    startTime?: number; // default: '0'
+    // End time for the rendering of this vnode
+    endTime?: number; // defualt: '-1'
+
 }
