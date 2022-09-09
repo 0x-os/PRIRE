bb99c2403a9b30782104c00436904b40629d6330
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-09 19:50:55 +0800

    added base-internal: base

---
+++ src/base-internals/base.d.ts
@@ -0,0 +1,19 @@
+export type Key = string | number | any;
+export type RefObject<T> = { current: T | null };
+export type RefCallback<T> = ( instance: T | null ) => null;
+export type Ref<T> = RefObject<T> | RefCallback<T>;
+
+export interface Attributes {
+    key?: Key;
+    jsx?: boolean;
+}
+
+export interface ClassAttributes<T> extends Attributes {
+    ref?: Ref<T>;
+}
+
+export interface ErrorInfo {
+    componentStack?: string;
+}
+
+export type RenderableProps<P, RefType = any> P & Readonly<Attributes & { children: ComponentChildren; red?: Ref<RefType>}>;
