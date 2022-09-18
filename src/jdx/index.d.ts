8e47e51f94a5de18755be901b23e00f7cedef1b3
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-18 13:38:42 +0800

    added jsx: index

--- src/jdx/index.d.ts
+++ src/jdx/index.d.ts
@@ -1,1 +1,21 @@
+import * as CSSProperties from './CSSProperties';
+import * as DOMAttributes from './DOMAttributes';
+import * as HTMLAttributes from './HTMLAttributes';
+import * as SVGAttributes from './SVGAttributes';
+import * as otherAttributes from './otherAttributes';
+import * as eventHandlers from './eventHandlers';
+import * as targetedEvents from './targetedEvents';
+import * as intrinsicElements from './intrinsicElements';
+import * as elements from './elements';
 
+export namespace JSXInternal {
+	export CSSProperties;
+	export DOMAttributes;
+	export HTMLAttributes;
+	export SVGAttributes;
+	export otherAttributes;
+	export eventHandlers;
+	export targetedEvents;
+	export elements;
+	export intrinsicElements;
+}
