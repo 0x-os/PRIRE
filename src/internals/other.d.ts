5878882c2497f823e8514b341a91881982c52b84
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-20 13:25:08 +0800

    added internal: other

---
+++ src/internals/other.d.ts
@@ -0,0 +1,26 @@
+import * as prire from '../base-internals';
+
+type RefObject<T> = { current: T | null };
+type RefCallback<T> = { (instance: T | null): void; current: undefined };
+type Ref<T> = RefObject<T> | RefCallback<T>;
+
+export interface DevSource {
+	fileName: string;
+	lineNumber: number;
+}
+
+export interface ErrorInfo {
+	componentStack?: string;
+}
+
+export interface prireContext extends prire.Context<any> {
+	_id: string;
+	_defaultValue: any;
+}
+
+export interface prireElement extends HTMLElement {
+	_children?: VNode<any> | null;
+	_listeners?: Record<string, (e: Event) => void>;
+	ownerSVGElement?: SVGElement | null;
+	data?: string | number; // From Text node
+}
