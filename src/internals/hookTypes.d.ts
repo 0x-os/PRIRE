7b512783c04c23e8822c50a9f4e7853fcdaf9671
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-20 13:25:57 +0800

    added internal: hookTypes

---
+++ src/internals/hookTypes.d.ts
@@ -0,0 +1,13 @@
+export enum HookType {
+	useState = 1,
+	useReducer = 2,
+	useEffect = 3,
+	useLayoutEffect = 4,
+	useRef = 5,
+	useImperativeHandle = 6,
+	useMemo = 7,
+	useCallback = 8,
+	useContext = 9,
+	useErrorBoundary = 10,
+	useDebugvalue = 11
+}
