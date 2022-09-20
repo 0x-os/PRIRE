541620b3f5f3f4d3300e7ffc7c3d535d8c1cb3c2
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-20 19:00:03 +0800

    added internal: index

---
+++ src/internals/index.d.ts
@@ -0,0 +1,11 @@
+import * as Components from './components';
+import * as HookTypes from './hookTypes';
+import * as Options from './options';
+import * as VNode from './vnode';
+import * as Other from './other';
+
+export Components;
+export HookTypes;
+export Options;
+export VNode;
+export Other;
