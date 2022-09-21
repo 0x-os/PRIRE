728ced9f40b979a6d1f5c647ff49639d946a7200
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-21 13:10:02 +0800

    added component: component.setState

--- src/component/component.js
+++ src/component/component.js
@@ -8,3 +8,33 @@ export function Component(props, context) {
 	this.props = props;
 	this.context = context;
 }
+
+/**
+ * Update component state and schedule a re-render.
+ * @this {import('../internals/').Component}
+ * @param {object | ((s: object, p: object) => object)} update A hash of state properties to update with new values or a function that given the current state and props returns a new partial state
+ * @param {() => void} [callback] A function to be called once component state is updated
+ */
+Component.prototype.setState = function(update, callback) {
+	let s;
+	if (this._nextState != null && this._nextState !== this.state) {
+		s = this._nextState;
+	} else {
+		s = this._nextState = assign({}, this.state);
+	}
+
+	if (typeof update == 'function') {
+		update = update(assign({}, s), this.props);
+	}
+
+	if (update) {
+		assign(s, update);
+	}
+
+	if (update == null) return;
+
+	if (this._vnode) {
+		if (callback) this._renderCallbacks.push(callback);
+		enqueueRender(this);
+	}
+};
