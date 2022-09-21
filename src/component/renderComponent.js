a060e5b97ea2c0fee5de6d01b320f31450f97991
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2022-09-21 13:20:55 +0800

    added component: renderComponent

---
+++ src/component/renderComponent.js
@@ -0,0 +1,32 @@
+/**
+ * Trigger in-place re-rendering of a component.
+ * @param {import('../internals/').component} component The component to rerender
+ */
+function renderComponent(component) {
+	let vnode = component._vnode,
+		oldDom = vnode._dom,
+		parentDom = component._parentDom;
+
+	if (parentDom) {
+		let commitQueue = [];
+		const oldVNode = assign({}, vnode);
+		oldVNode._original = vnode._original + 1;
+
+		diff(
+			parentDom,
+			vnode,
+			oldVNode,
+			component._globalContext,
+			parentDom.ownerSVGElement !== undefined,
+			vnode._hydrating != null ? [oldDom] : null,
+			commitQueue,
+			oldDom == null ? getDomSibling(vnode) : oldDom,
+			vnode._hydrating
+		);
+		commitRoot(commitQueue, vnode);
+
+		if (vnode._dom != oldDom) {
+			updateParentDomPointers(vnode);
+		}
+	}
+}
